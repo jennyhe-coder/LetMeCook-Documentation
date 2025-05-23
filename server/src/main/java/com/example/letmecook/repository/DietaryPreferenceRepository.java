661f19db7fd835516dca140952b15ed53d00811f
Carmen Lam <148829232+Carmen-jx@users.noreply.github.com>
2025-05-23 11:13:19 -0400

    Revert "Tai huynh backend"

--- server/src/main/java/com/example/letmecook/repository/DietaryPreferenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.letmecook.repository;
-
-import com.example.letmecook.model.DietaryPreference;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-interface DietaryPreferenceRepository extends JpaRepository<DietaryPreference, UUID> {
-}
