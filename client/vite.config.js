73479d29b67187a7ad6a1e8c00cbf81d0ec8afa9
Van Tai Huynh <vhuynh16@myseneca.ca>
2025-05-26 20:28:37 -0400

    change client to run in sever

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: ['letmecook.ca', 'www.letmecook.ca'],
     proxy: {
-      "/api": {
+      "/api/letmecook.ca": {
         target: "http://localhost:8080/",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        rewrite: (path) => path.replace(/^\/api\/letmecook\.ca/, "/api"),
       },
     },
   },
 });
- 
