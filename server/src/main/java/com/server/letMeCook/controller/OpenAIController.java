70b10ec3dfde578a13581c847018f91a7286f469
Tai Huynh <hvtai.it@gmail.com>
2025-08-07 14:46:44 -0400

    fix openCV call

--- server/src/main/java/com/server/letMeCook/controller/OpenAIController.java
+++ server/src/main/java/com/server/letMeCook/controller/OpenAIController.java
@@ -7,14 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api/opencv/")
 class OpenAIController {
     private final OpenAIService openAIService;
